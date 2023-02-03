e79abf06b6b3a44e52e62b4b531fb81b7251a33e
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2023-02-03 15:13:51 +0100

    updated the navbar

--- components/Navbars/WebsiteNavbar.jsx
+++ components/Navbars/WebsiteNavbar.jsx
@@ -63,7 +63,7 @@ const WebsiteNavbar = () => {
               </div>
               {isMenuOpen && (
                 <div className="absolute z-50 top-0 left-0 w-full">
-                  <div className="p-5 bg-slate-200 dark:bg-slate-700 dark:border-none border rounded shadow-sm">
+                  <div className="p-5 bg-light border rounded shadow-sm">
                     <div className="flex items-center justify-between mb-4">
                       <div>
                         <Link href="/" className="text-2xl text-primary font-semibold lg:font-bold">
@@ -77,7 +77,7 @@ const WebsiteNavbar = () => {
                           className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-primary focus:bg-primary focus:outline-none focus:shadow-outline"
                           onClick={() => setIsMenuOpen(false)}
                         >
-                          <svg className="w-5 text-light" viewBox="0 0 24 24">
+                          <svg className="w-5 text-primary hover:text-white" viewBox="0 0 24 24">
                             <path
                               fill="currentColor"
                               d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
