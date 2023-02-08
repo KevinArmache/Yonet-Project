eeac25b7c334348d562952c8962a8c87aa247d10
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2023-02-08 12:56:43 +0100

    updated the title font size

--- components/HomePageSections/Hero.jsx
+++ components/HomePageSections/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <section className='bg-white'>
       <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 min-h-full flex flex-col items-center justify-center'>
         <div className='max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12'>
-          <h1 className='max-w-lg mb-6 font-sans text-7xl font-extrabold leading-none tracking-tight text-primary sm:text-9xl md:mx-auto'>
+          <h1 className='max-w-xl mb-6 font-sans text-7xl font-extrabold leading-none tracking-tight text-primary sm:text-8xl md:mx-auto'>
             Simplifie toi la Vie
           </h1>
           <p className="text-base text-gray-700 md:text-lg">
