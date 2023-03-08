280101aaf9092eed337b4b93d1bd2fa760f8ab2a
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2023-03-08 15:34:52 +0100

    wokring on create category

--- layouts/AdminLayout.jsx
+++ layouts/AdminLayout.jsx
@@ -21,6 +21,13 @@ const AdminLayout = ({ children, title, description }) => {
             </h1>
           </div>
         </header>
+        <main>
+          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <>
+              { children }
+            </>
+          </div>
+        </main>
         <WebsiteFooter />
       </div>
     </>
--- src/pages/admin/categories/index.jsx
+++ src/pages/admin/categories/index.jsx
@@ -1,8 +1,59 @@
+import Button from 'components/Buttons/Button';
+import AdminLayout from 'layouts/AdminLayout';
+import { getSession } from 'next-auth/react';
 import React from 'react'
+import { prisma } from 'utils/prisma';
+
+export async function getServerSideProps(context) {
+  // Check if user is authenticated
+  const session = await getSession(context);
+
+  // If not, redirect to the homepage
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  } else {
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: session.user.email
+      }
+    })
+
+    if (user.isAdmin) {
+      
+      return {
+        props: {
+          
+        },
+      };
+    } else {
+      return {
+        redirect: {
+          destination: '/403',
+          permanent: false,
+        },
+      };
+    }
+
+  }
+
+}
 
 const ServiceCategories = () => {
   return (
-    <div>ServiceCategories</div>
+    <AdminLayout title="Catégories de service">
+      <div className="flex items-start mx-4 w-full space-x-2">
+        <Button
+          text="Créer une nouvelle catégorie"
+          url="/admin/categories/create"
+        />
+      </div>
+    </AdminLayout>
   )
 }
 
