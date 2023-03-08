c23e9ec2a0e0cb4a3166b87e983e5b58721e86f1
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2023-03-08 14:23:56 +0100

    worked on admin navbar

---
+++ layouts/AdminLayout.jsx
@@ -0,0 +1,30 @@
+import WebsiteFooter from 'components/Footers/WebsiteFooter'
+import AdminNavbar from 'components/Navbars/AdminNavbar'
+import Head from 'next/head'
+import React from 'react'
+
+const AdminLayout = ({ children, title, description }) => {
+  return (
+    <>
+      <Head>
+        <title>
+          {`${title} - Yonet Administration`}
+        </title>
+        {description && <meta name="description" content={description} />}
+      </Head>
+      <div className="min-h-full">
+        <AdminNavbar />
+        <header className="bg-white shadow pt-16">
+          <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-lg lg:text-3xl font-bold tracking-tight text-gray-900">
+              {title}
+            </h1>
+          </div>
+        </header>
+        <WebsiteFooter />
+      </div>
+    </>
+  )
+}
+
+export default AdminLayout
