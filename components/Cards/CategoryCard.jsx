8c72d52c138107da26a0e22b2d944f5a77e7ea8c
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2023-03-10 15:47:17 +0100

    fetching categories from API

--- components/Cards/CategoryCard.jsx
+++ components/Cards/CategoryCard.jsx
@@ -1,16 +1,19 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const CategoryCard = ({ category }) => {
   return (
-    <div className='hover:text-primary ease-in duration-300 text-secondary hover:cursor-pointer'>
-      <Image width={1080} height={720} src="/asset/images/beatmaker.jpg" alt="" className='w-full object-cover rounded-md' />
-      <div className='mt-2'>
-        <p className='font-light text-base'>
-          Beat maker
-        </p>
+    <Link href={`/categories/${category.slug}`}>
+      <div className='hover:text-primary ease-in duration-300 text-secondary hover:cursor-pointer'>
+        <Image width={1080} height={720} src={category.image} alt="" className='w-full object-cover rounded-md' />
+        <div className='mt-2'>
+          <p className='font-light text-base'>
+            {category.name}
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,12 +4,24 @@ import Categories from "components/HomePageSections/Categories";
 import HowItWorks from "components/HomePageSections/HowItWorks";
 import About from "components/HomePageSections/About";
 import Action from "components/HomePageSections/Action";
+import axios from "axios";
 
-export default function Home() {
+export async function getServerSideProps(context) {
+  // return the categories
+
+  const { data } = await axios.get(`${process.env.NEXTAUTH_URL}/api/admin/categories`);
+  return {
+    props: {
+      categories: data.data
+    }
+  }
+}
+
+export default function Home({ categories }) {
   return (
     <WebsiteLayout title="Les meileurs freelancers dans la création de contenus">
       <Hero />
-      <Categories />
+      <Categories categories={categories} />
       <HowItWorks />
       <About />
       <Action />
