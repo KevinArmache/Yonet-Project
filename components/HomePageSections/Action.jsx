4bcb29240d42414eb2344ae751f0b8f45b4c2199
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2023-03-13 13:38:15 +0100

    worked on main pages

--- components/HomePageSections/Action.jsx
+++ components/HomePageSections/Action.jsx
@@ -14,7 +14,7 @@ const Action = () => {
 
   return (
     <section className='bg-primary'>
-      <div className='px-4 py-6 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-12 flex flex-col lg:flex-row items-center justify-center space-x-6'>
+      <div className='px-4 py-6 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-12 flex flex-col lg:flex-row items-center justify-center lg:space-x-6 space-x-0 space-y-4 lg:space-y-0'>
         <div className='lg:basis-2/3 text-center lg:text-justify'>
           <h1 className='my-8 font-sans text-3xl font-extrabold leading-none tracking-tight text-white sm:text-5xl'>
             Vous avez du talent à vendre ?<br/>
@@ -24,14 +24,14 @@ const Action = () => {
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
           </p>
         </div>
-        <div className='w-full md:basis-1/3 mt-8 lg:mt-0 flex items-center justify-center md:space-x-8 space-x-0'>
+        <div className='basis-1/3 '>
           {isLoadingUser ? (
             <p className='text-white'>...</p>
           ) : (
-            <>
-              {router.pathname.includes('pour-les-vendeurs') || router.pathname.includes('app') ? null : <Button text="Pour les vendeurs" url="/pour-les-vendeurs" variant="outline" />}
+            <div className='w-full flex flex-col lg:flex-row lg:space-x-2 items-center justify-center space-y-2 lg:space-y-0'>
+              {router.pathname.includes('pour-les-vendeurs') || router.pathname.includes('app') ? null : <Button text="Vendeurs" url="/pour-les-vendeurs" variant="outline" />}
               <Button text={user ? 'Créer votre compte vendeur' : 'Commencer'} url={user ? '/app/vendeur/profil' : '/connexion'} variant="light" />
-            </>
+            </div>
           )}
         </div>
       </div>
--- src/pages/app/vendeur/profil.jsx
+++ src/pages/app/vendeur/profil.jsx
@@ -2,6 +2,29 @@ import React from 'react'
 import SellerProfileForm from '../../../../components/Forms/SellerProfileForm'
 import WebsiteLayout from 'layouts/WebsiteLayout'
 import Breadcrumb from '../../../../components/Navbars/Breadcrumb';
+import { getSession } from 'next-auth/react';
+
+export async function getServerSideProps(context) {
+  // Check if user is authenticated
+  const session = await getSession(context);
+
+  // If not, redirect to the homepage
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      props: {
+
+      }
+    }
+  }
+
+}
 
 const ProfilVendeur = () => {
   const saveProfile = (data) => console.log(data); 
